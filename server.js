faa01c91930535acc445ca60e992d57c4b51993e
Chinnapat.N <69895436+Kkr1328@users.noreply.github.com>
2023-04-05 11:40:33 +0700

    Add chat service

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+// import
 const express = require("express");
-const app = express();
 const http = require("http");
 const cors = require("cors");
+const socketio = require("socket.io");
+const chat = require("./utilities/chat");
 
-app.use(cors()); // Add cors middleware
+const app = express();
+app.use(cors());
 
+const PORT = 9000;
 const server = http.createServer(app);
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-server.listen(4000, () => "Server is running on port 4000");
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+  transports: ["websocket"],
+  allowEIO3: true,
+});
+chat(io);
