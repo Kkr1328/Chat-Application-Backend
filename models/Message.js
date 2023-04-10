09613c1f077fb35080dc224aa7b458a2bf761e24
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-04-10 11:54:50 +0700

    Add id in message schema

--- models/Message.js
+++ models/Message.js
@@ -1,26 +1,37 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = new mongoose.Schema({
-  chat_id: {
-    type: mongoose.Types.ObjectId,
-    require: true,
+const MessageSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: mongoose.Types.ObjectId,
+      require: true,
+    },
+    user_id: {
+      type: mongoose.Types.ObjectId,
+      require: true,
+    },
+    chat_id: {
+      type: mongoose.Types.ObjectId,
+      require: true,
+    },
+    message: {
+      type: String,
+      require: true,
+    },
+    liked_users: {
+      type: Array(mongoose.Types.ObjectId),
+      default: [],
+    },
+    like: {
+      type: Number,
+      default: 0,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  message: {
-    type: String,
-    require: true,
-  },
-  liked_users: {
-    type: Array(mongoose.Types.ObjectId),
-    default: [],
-  },
-  like: {
-    type: Number,
-    default: 0,
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { _id: false }
+);
 
 module.exports = mongoose.model("Message", MessageSchema);
